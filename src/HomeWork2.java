c1c167a4a3f3395ace73a5d38d2d05796a9b755d
Temirlan-tech <t.toktosunov@list.ru>
2020-09-09 17:55:23 +0600

    Сделал ДОП ДЗ

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,16 +1,18 @@
+import java.util.Random;
+
 public class HomeWork2 {
 
 
     public static void main(String[] args) {
-        System.out.println(myAge(22,35));
-        System.out.println(myAge(15,29));
-        System.out.println(myAge(67,40));
-        System.out.println(myAge(9,16));
-        System.out.println(myAge(33,19));
+        System.out.println(myAge(generateRandomeAge(), 35));
+        System.out.println(myAge(generateRandomeAge(), 29));
+        System.out.println(myAge(generateRandomeAge(), 40));
+        System.out.println(myAge(generateRandomeAge(), 16));
+        System.out.println(myAge(generateRandomeAge(), 19));
 
     }
 
-    public static String myAge (int age, int temp){
+    public static String myAge(int age, int temp) {
         if (age > 20 && age < 45 && temp > -20 && temp < 30) {
             return "Можно идти гулять";
         } else if (age < 20 && temp > 0 && temp < 28) {
@@ -22,6 +24,12 @@ public class HomeWork2 {
         }
 
 
+    }
+
+    public static int generateRandomeAge() {
+        Random random = new Random();
+        int i = random.nextInt(100) + 1;
+        return i;
     }
 
 }
